05b227f5973f377f6a7d9de810e3c56796942746
menna1210 <mennaassem1210@gmail.com>
2024-10-28 02:34:36 +0300

    new commit

---
+++ landing page/script.js
@@ -0,0 +1,10 @@
+let img =document.querySelector(".img")
+let container =document.querySelector(".container")
+
+function changePhone(phone){
+img.src=phone;
+
+}
+function colors(color){
+container.style.background =color;
+}
